e3d0082eb911bdab67ceb555ee3dd833bd57d1c3
anu-shree11 <67831286+anu-shree11@users.noreply.github.com>
2020-07-04 19:49:44 +0530

    created project1.cpp
    the program displays the size of various data types

---
+++ project1.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	char c;
+	int a;
+	float b;
+	double d;
+	cout<<"Size of char: "<<sizeof(c)<<" bytes"<<"\n"<<"Size of int: "<<sizeof(a)<<" bytes"<<"\n"<<"Size of float: "<<sizeof(b)<<" bytes"<<"\n"<<"Size of double: "<<sizeof(d)<<" bytes";
+}
