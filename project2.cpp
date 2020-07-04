1f253cee98f8447d4022bf9ece23d510607e5920
anu-shree11 <67831286+anu-shree11@users.noreply.github.com>
2020-07-04 19:58:41 +0530

    created project2.cpp
    the program displays the product of the two numbers entered by the user

---
+++ project2.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float a,b,ans;
+	cout<<"Enter two numbers:\n";
+	cin>>a>>b;
+	ans=a*b;
+	cout<<"The product of the numbers entered is: "<<ans;
+}
